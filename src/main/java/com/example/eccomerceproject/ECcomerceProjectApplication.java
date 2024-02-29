29f517e2950df9b089a83268ae861026ba488025
Nilesh Bahetwar <bahetwarnilesh2310@gmail.com>
2024-02-29 18:59:41 +0530

    firts commit

---
+++ src/main/java/com/example/eccomerceproject/ECcomerceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.eccomerceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECcomerceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECcomerceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eccomerceproject/ECcomerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eccomerceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECcomerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
